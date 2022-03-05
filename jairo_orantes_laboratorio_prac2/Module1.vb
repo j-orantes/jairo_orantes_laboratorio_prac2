3965b3f1dedba9f2b73f39567ec1463abaab8e70
Jairo Orantes <jairo.orantes@catolica.edu.sv>
2022-03-04 19:36:09 -0600

    a

---
+++ jairo_orantes_laboratorio_prac2/Module1.vb
@@ -0,0 +1,44 @@
+﻿Module Module1
+
+	Sub Main()
+		Dim opcion As Integer = 0
+		Dim num1, num2 As Decimal
+		Console.Write("Ingrese el primer número: ")
+		num1 = Console.ReadLine
+		Console.Write("Ingrese el segundo número: ")
+		num2 = Console.ReadLine
+		Console.WriteLine("¿Qué operacion desea realizar? | Suma = 1 | Resta = 2 | Producto = 3 | División = 4 | Salir = 5 |")
+		opcion = Console.ReadLine()
+		If opcion = 1 Then
+			Console.WriteLine()
+		Else
+			If opcion = 2 Then
+				suma()
+			Else
+				If opcion = 3 Then
+					Console.WriteLine("si")
+				Else
+					If opcion = 4 Then
+						Console.WriteLine("si")
+					Else
+						Console.WriteLine("si")
+
+					End If
+
+				End If
+			End If
+
+		End If
+
+	End Sub
+	Sub main()
+    Function
+
+	End Function
+	End Sub
+
+	End Sub
+
+
+
+End Module
